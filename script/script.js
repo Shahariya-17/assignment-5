96cc6a6a6cc3ec4a2a0f7d506a4729bd79402a5a
Shahariya-17 <shahariyarrakib780@gmail.com>
2025-03-02 11:55:48 +0600

    added mai section

--- script/script.js
+++ script/script.js
@@ -1,10 +1,40 @@
 
-document.getElementById('theme-btn').addEventListener('click',
-    function(event){
-        event.preventDefault();
+// document.getElementById('theme-btn').addEventListener('click',
+//     function(event){
+//         event.preventDefault();
 
-        function makeYellow(){
-            document.body.style.backgroundColor = 'yellow';
+//         function makeYellow(){
+//             document.body.style.backgroundColor = 'yellow';
+//         }
+//     }
+// )
+
+const buttons = document.getElementsByClassName('card-btn');
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function() {
+        alert('Board updated successfully');
+        const firstBtn = document.getElementById('first-btn').innerText;
+        const convertedNumber = parseInt(firstBtn, 10); 
+        const secondBtn = document.getElementById('second-btn').innerText;
+        const convertedSecondNumber = parseInt(secondBtn);
+
+        
+        
+
+        if (!isNaN(convertedSecondNumber <= 0 &&   convertedNumber)) {
+            const newCompletedTask = convertedNumber + 1;
+            document.getElementById('first-btn').innerText = newCompletedTask;
+            const newTask = convertedSecondNumber -1;
+            document.getElementById('second-btn').innerText = newTask; 
+
+            
         }
-    }
-)
+    })
+}
+
+document.getElementById('discover-btn').addEventListener('click', function(){
+    window.location.href = "./main.html"
+})
+
+
---
+++ script/toggle.js
@@ -0,0 +1,6 @@
+document.getElementById('issue-box').style.display = 'none';
+document.getElementById('dark-box').style.display = 'none';
+document.getElementById('page-box').style.display = 'none';
+document.getElementById('emoji-box').style.display = 'none';
+document.getElementById('api-box').style.display = 'none';
+document.getElementById('job-box').style.display = 'none';
